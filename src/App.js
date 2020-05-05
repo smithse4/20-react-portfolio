bcc96a8415cfde04c38f52a8a785d1d6a622cddc
Stephanie smith <smithse4@gmail.com>
2020-05-04 21:53:51 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import About from "./containers/About/About";
+import Contact from "./containers/Contact/Contact";
+import Portfolio from "./containers/Portfolio/Portfolio";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <About />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
